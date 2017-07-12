2977dd4b0d5aaf1b9fc2f8fc6f48e7720bc151a7
Hare Kumar <hare.kumar@freecharge.com>
2017-07-12 18:27:09 +0530

    Very first commit

---
+++ src/main/java/projecteuler/projecteuler/App.java
@@ -0,0 +1,13 @@
+package projecteuler.projecteuler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
